b47677470e65b3aa54c2c8302fa849e876939362
横田昴輝 <kokiyokotakoki@gmail.com>
2024-12-22 13:51:52 +0900

    1章完

---
+++ src/tmp_implementation/moneyTest.ts
@@ -0,0 +1,20 @@
+export class Dollar {
+    amount: number;
+    constructor(amount: number) {
+        this.amount = amount;
+        
+    }
+    times(multiplier: number) {
+        this.amount *= multiplier;
+        
+    }
+}
+export class MoneyTest {
+    five: Dollar;
+    constructor() {
+        this.five = new Dollar(5);
+    }
+    testMultiplication() {
+        this.five.times(2);
+    }
+}
