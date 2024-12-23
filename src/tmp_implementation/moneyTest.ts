b5d3e85bcf2f6244063f0be79523d09c11bafe3d
横田昴輝 <kokiyokotakoki@gmail.com>
2024-12-24 00:19:37 +0900

    4章完

--- src/tmp_implementation/moneyTest.ts
+++ src/tmp_implementation/moneyTest.ts
@@ -1,13 +1,15 @@
 export class Dollar {
-    amount: number;
+    private amount: number;
     constructor(amount: number) {
         this.amount = amount;
-        
     }
     times(multiplier: number) {
         return this.amount * multiplier;
     }
     equals(obj: Dollar) {
         return this.amount === obj.amount;
+    }
+    getAmount(): number {
+        return this.amount;
     }
 }
--- test/tmp_implementation/moneyTest.test.ts
+++ test/tmp_implementation/moneyTest.test.ts
@@ -3,10 +3,8 @@ import { Dollar } from "../../src/tmp_implementation/moneyTest";
 describe("MeneyTest", () => {
     it("testMultiplication", () => {
         const five = new Dollar(5);
-        let product: number = five.times(2);
-        expect(product).toEqual(10);
-        product = five.times(3);
-        expect(product).toEqual(15);
+        expect(new Dollar(10).getAmount()).toEqual(five.times(2));
+        expect(new Dollar(15).getAmount()).toEqual(five.times(3));
     });
     it("testEquality", () => {
         const five = new Dollar(5);
