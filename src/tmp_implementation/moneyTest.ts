5c2997f1b375e532cff51f4a42959b39b19fe43e
横田昴輝 <kokiyokotakoki@gmail.com>
2024-12-24 01:53:26 +0900

    5章完

--- src/tmp_implementation/moneyTest.ts
+++ src/tmp_implementation/moneyTest.ts
@@ -13,3 +13,19 @@ export class Dollar {
         return this.amount;
     }
 }
+
+export class Franc {
+    private amount: number;
+    constructor(amount: number) {
+        this.amount = amount;
+    }
+    times(multiplier: number) {
+        return this.amount * multiplier;
+    }
+    equals(obj: Franc) {
+        return this.amount === obj.amount;
+    }
+    getAmount(): number {
+        return this.amount;
+    }
+}
